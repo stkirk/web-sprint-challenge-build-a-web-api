cfc2eae77e4e1dea2ed291828faf1f702624eb17
stkirk <shaanekirk@gmail.com>
2021-10-09 15:08:18 -0400

    get project actions endpoint functional with id checking middleware

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -53,4 +53,12 @@ router.delete("/:id", checkProjectById, (req, res, next) => {
     .catch(next);
 });
 
+router.get("/:id/actions", checkProjectById, (req, res, next) => {
+  Projects.getProjectActions(req.params.id)
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch(next);
+});
+
 module.exports = router;
