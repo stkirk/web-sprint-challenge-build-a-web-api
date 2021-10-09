bc36d25f629fb65b164524dde6291668cb6be086
stkirk <shaanekirk@gmail.com>
2021-10-09 15:20:00 -0400

    get actions by id working with action id validating middleware

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,21 @@
 // add middlewares here related to actions
+const Actions = require("./actions-model");
+
+const checkActionById = (req, res, next) => {
+  console.log("verifying action id...");
+  const { id } = req.params;
+  Actions.get(id)
+    .then((action) => {
+      if (action) {
+        req.action = action;
+        next();
+      } else {
+        next({ status: 404, message: `no action with id ${id}` });
+      }
+    })
+    .catch(next);
+};
+
+module.exports = {
+  checkActionById,
+};
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,6 +1,20 @@
 // Write your "actions" router here!
 const express = require("express");
+const { checkActionById } = require("./actions-middlware");
+const Actions = require("./actions-model");
 
 const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  Actions.get()
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch(next);
+});
+
+router.get("/:id", checkActionById, (req, res, next) => {
+  res.status(200).json(req.action);
+});
 
 module.exports = router;
